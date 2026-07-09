f6daec5c817964acf5b3b3dd371fd08176be91e8
irfanfadhli <irfanfadhlim@gmail.com>
2026-07-10 00:20:48 +0700

    edit layout

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/logo.svg';
 	import { Toaster } from 'svelte-sonner';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={logo} /></svelte:head>
 <Toaster position="bottom-right" richColors />
 <div
 	class="flex min-h-screen items-center justify-center bg-gradient-to-br from-green-50 to-emerald-100 p-4"
