2b8dbc2aa003ed7f49b8a107d1cd08e5f8b80e4f
irfanfadhli <irfanfadhlim@gmail.com>
2026-07-14 21:47:07 +0700

    update keywords

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,11 @@
-const ORIGIN = import.meta.env.ORIGIN || 'http://localhost:5173';
+const ORIGIN = import.meta.env.ORIGIN || 'https://www.tapioleaf.my.id';
 
 export const siteConfig = {
 	name: 'CV TapioLeaf',
 	description:
 		'Produsen tepung tapioka berkualitas tinggi. Mengolah singkong pilihan menjadi tepung tapioka premium untuk kebutuhan industri dan rumah tangga.',
 	keywords:
-		'tepung tapioka, tapioka premium, cv tapioleaf, tepung singkong, pati singkong, tapioka industri, tepung tapioka pati, tapioka food grade',
+		'cv tapioleaf, tepung tapioka, jual tepung tapioka, produsen tepung tapioka, tepung tapioka Pati',
 	url: new URL(ORIGIN),
 	ogImage: new URL('/img/og.png', ORIGIN),
 	googleVerification: 'OihcPITN_69JSx4bq4ntPc7LIuXFpUxgFvgC_U-vOPg',
